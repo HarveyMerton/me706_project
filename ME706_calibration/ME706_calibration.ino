4f8ed6cc69b404b8338c755aab9cfb1905fa1b10
Harvey Merton <harveymerton@outlook.com>
2021-03-14 17:17:42 +1300

    Added working Gyro library
    Gyro library now works with interrupts to integrate gyro data.
    Still needs LPF implemented.

--- ME706_calibration/ME706_calibration.ino
+++ ME706_calibration/ME706_calibration.ino
@@ -5,100 +5,80 @@
  *  Date: 10/03/21
  */
 
-#include <avr/io.h>
-#include <avr/interrupt.h>
+#include <Gyro.h>
+#include <Math.h>
 
 //PINs setup
 #define PIN_GYRO_DAT 3 //Analogue 3 
 
-//Gyroscope calibration
-#define AV_CC 5.0 //Analogue supply voltage (V)
-#define ADC_MAX 1023 //Maximum output value of ADC converter
-
-#define GYRO_SENS 0.007 //Gyro sensitivity (V/dps)
-#define GYRO_RATE_THRESHOLD = 1.5 //Rotation rate less than this will be ignored - drift
-#define GYRO_Ts 0.1 //0.1 sec sampling period
-
-float V_NULL = 2.5; //Null voltage (V) - voltage when the gyro reading is 0
-long gyroAngle = 0; //Total angle tracked by gyro
-
+//Global vars setup
+Gyro gyro = Gyro(PIN_GYRO_DAT);
 
 void setup() {
   cli();
-  
-  //SET UP TC1 for gyro integration
-  PRR0 &= ~(1<<PRTIM1); //Turn off power reduction for TC1
-  
-  //Set timer/counter1 to CTC mode
-  TCCR1A = 0; 
-  TCCR1B |= 1<<WGM12; 
-  
-  //Set reset value 
-  OCR1A = 780; 
-
-  //Set prescalar and start clock
-  TCCR1A |= (1<<CS12) | (1<<CS10);
-
-  //Enable interrupt
-  TIMSK1 |= 1<<OCIE1A;
-
-  
+   
   //Set up serial output
   Serial.begin(115200);
 
-  //Set up pins 
-  pinMode(PIN_GYRO_DAT,INPUT);
-  
-  //Calibrate
-  //Serial.println("Calibrating Gyro");
-  //GYRO_calibrate();
-
+  //Set up gyro
+  gyro.timerSetup();
+  gyro.calibrate(); 
+  gyro.countOn();
   sei();
 }
 
+int i = 0;
+int j = 0;
 void loop() {
- asm("nop");
-}
-
-
-void gyro_calibrate()
-{
-  //Find the null voltage
-  long gyroSum = 0;
-  
-  for(int i = 0; i<100; i++){ //Average readings over 100 values
-    gyroSum += analogRead(PIN_GYRO_DAT);
-    delay(5);
+  //For testing the frequency of interrupts
+  if(i == 0){
+    //testFreq();
+    //Serial.println(findOC1A());
   }
+
+  //For testing the ability of the gyro to measure
+  //testMeasure();
+  asm("nop");
+}
+
+//For testing the frequency of the interrupts
+void testFreq(){ 
+  gyro.countOn();
+  delay(1000);
+  gyro.countOff();
+  i++;
+  Serial.print("Number of interrupts: "); 
+  Serial.println(j);
+}
+
+//For testing the measurement output of the gyro
+void testMeasure(){ 
+  Serial.print("Start ang: ");
+  Serial.println(gyro.getAng());
+  delay(2000); 
+
+  gyro.countOn(); 
+  delay(1000); 
+
+  gyro.countOff();
+  Serial.print("End ang: "); 
+  Serial.println(gyro.getAng());
   
-  V_NULL = (gyroSum/100)*(AV_CC/ADC_MAX);
-  
+  gyro.reset();
+  Serial.print("After reset: "); 
+  Serial.println(gyro.getAng());
 }
 
-
-float gyro_currentAngle() 
-{ 
-  
-}
-
-
-//Toggle gyro integration on
-void gyro_countOn(){ 
-  TIMSK1 |= 1<<OCIE1A;
-}
-
-//Toggle gyro integration off
-void gyro_countOff(){ 
-  TIMSK1 &= ~(1<<OCIE1A);
-}
-
-//Reset gyro value 
-void gyro_reset(){ 
-  gyroAngle = 0;
+//Function to find the correct OC1A setting for a desired sampling frequency
+float findOC1A(){ 
+  return ((16000000/(1024.0*GYRO_Fs))-1);
 }
 
 //Interrupt for tc1 - get gyro value for integration
 ISR(TIMER1_COMPA_vect){ 
-  gyroAngle += analogRead(PIN_GYRO_DAT)*GYRO_Ts;   ///////HEREEEEEE AND CURRENT ANGLEEEEEEEEE
-  Serial.println(gyroAngle);
+  //Serial.println(1.0/GYRO_Fs);
+  //Serial.println(gyro.getAng() + gyro.readAngRate()*(1.0/GYRO_Fs));
+  gyro.setAng(gyro.getAng() + gyro.readAngRate()*(1.0/GYRO_Fs));  
+  Serial.println(gyro.getAng()); 
+  j++;
 }
