c467dd02c8213c5ec28bb92071e0d81224f99881
DrPedro <52388741+DrPedro@users.noreply.github.com>
2021-04-15 11:34:15 +1200

    Complete Code; Only Requires Controller Tuning

--- Running code/RobotBaseCodes/RunningCode.ino
+++ Running code/RobotBaseCodes/RunningCode.ino
@@ -3,42 +3,38 @@ enum RUNNINGSTATE {SETUP, STRAIGHT, ROTATION, STOP};
 RUNNINGSTATE RunningState = SETUP;
 
 //Motor Powers
-//double speed_val_left = 0;
-//double speed_val_right = 0;
+double speed_val_left = 0;
+double speed_val_right = 0;
+double speed_val_rotate = 0;
+double speed_val_setup = 150;
 
-////Running Code Variables
+//Power Allocation
+double max_power_staight = 500;
+double min_power_straight = 100;
+double power_side = 250;
+double power_difference = 100;
+double max_power_rotate = 250;
+double min_power_rotate = 20;
+
+//Running Code Variables
 int turnCount = 0;
-//int frontTarget = 80;
-//int leftTarget = 285; //inverse response atm
-//int rotateTarget = 90;
-//double l_error;
-//double f_error;
-//double IR_error;
-//double error_total;
-//
-////Straight Controller
-//double S_Kp = 0.005;
-//double S_Ki = 0.0;
-//double S_Kd = 0.0;
-//
-////Forward Controller
-//double F_Kp = 7.0;
-//double F_Ki = 0.005;
-//double F_Kd = 0.0;
-//
-////Rotation Controller
-//double R_Kp = 7.0;
-//double R_Ki = 0.005;
-//double R_Kd = 0.0;
-//
-//float l_target = 70;
-//float f_target = 80;
-//
-//float turn_error = 0;
-//float turn_error_total = 0;
+int frontTarget = 140;
+int sideTarget = 90;
+int rotateTarget = 90;
+double sideError;
+double frontError;
+double differenceError;
+double rotateError;
+
+//Controller Gains
+double F_Kp = 4;  //Forward Controller
+double S_Kp = 20; //Side Controller
+double D_Kp = 20; //Difference Controller
+double R_Kp = 10;  //Rotation Controller
+
 
 void TaskMain() {
-  Serial.println(RunningState);
+  //FSM for running all tasks required
   switch (RunningState) {
     case SETUP:
       Setup();
@@ -57,113 +53,73 @@ void TaskMain() {
   }  
 }
 
-int target = 90; //Decrease = further out, increase = closer //350 380
-int forward_target = 150; //Decrease = further out, increase = closer //250 280
-float k = 15; //30 15 15 15
-float ki = 0.005; //0 0.1 0.01 0.005
-int error_total = 0;
 
 void Setup() {
-  //Align robot so its 150mm away from the wall on the left and 150mm away from the wall on the back
-  double errorLeft = IR_LF.getReading() - IR_LR.getReading();
-  double errorToWall = IR_LF.getReading() - target;
-    if (abs(errorLeft) > 7.5 || IR_LF.getReading() >= 130 || IR_LR.getReading() >= 130) {
-        ccw();
-    }
-    else if (errorToWall > 0) {
-      strafe_left();
-    }
-    else {
-      stop();
-      RunningState = STRAIGHT;
-    }
+  //Aligns the side of the robot with the wall
+  speed_val = speed_val_setup;  //Increases motor speed
+
+  //Error calculation
+  sideError = IR_LF.getReading() - sideTarget;
+  differenceError = IR_LF.getReading() - IR_LR.getReading();
+
+  //Logic to get robot properly aligned for driving straight
+  if (IR_LF.getReading() > 130 && IR_LR.getReading() > 130) {ccw();}
+  else if (sideError > 0 && abs(differenceError) < 10) {strafe_left();}
+  else if (differenceError > 2) {ccw();}
+  else if (differenceError < -2) {cw();}
+  else {
+    //FSM state exit condition
+    stop();
+    RunningState = STRAIGHT;
+  }
 }
 
-int error = 0;
-int f_error = 0;
 
 void Straight() {
- static unsigned long previous_millis;
+  //Implements three different P controllers in order to drive the robot straight with minimal deviation
+  //Error calculation
+  frontError = IR_FR.getReading() - frontTarget;
+  sideError = IR_LF.getReading() - sideTarget;
+  differenceError = IR_LF.getReading() - IR_LR.getReading();
 
-  //MY CODE
+  //Controller power calculation
+  double power_front = constrain(F_Kp*frontError, min_power_straight, max_power_staight);
+  double power_side = constrain(S_Kp*sideError, -power_side, power_side) + constrain(D_Kp*differenceError, -power_difference, power_difference);
 
-  error = IR_LF.getReading() - target;
-  f_error =  IR_FR.getReading() - forward_target;
-
+  //Motor speeds
+  speed_val_left = 1500 + power_front - power_side;
+  speed_val_right = 1500 - power_front - power_side;
   
-  //error_total = constrain((error_total + error), -20, upper);
-   error_total = (error_total + error);
-
-  //Serial.println(error_total);
-
-  int speed_val_left = 1750 - error * k - ki*error_total;
-  int speed_val_right = 1250 - error * k - ki*error_total;
-
-  if (error > 5) {
-
-      int speed_val_left = 1500 - error * k - ki*error_total;
-      int speed_val_right = 1500 - error * k - ki*error_total;
+  if (frontError > 0) {
+    SetMotorSpeed(speed_val_left, speed_val_right);
   }
-
-  
-  if (f_error < 0) {
+  else {
+    //FSM state exit condition
+    stop();
     GYRO.setAngle(0);
-    GYRO.initialise();
-
-    if (turnCount >= 3) {
-      RunningState = STOP;
-    } else {
-      RunningState = ROTATION;
-    }
-      
-            
-  } else { //Move forward and keep straight
-
-    if (speed_val_left > 2000) {
-      speed_val_left = 2000;
-    }
-  
-    if (speed_val_left < 1250) {
-      speed_val_left = 1250;
-    }
-    
-    if (speed_val_right > 1750) {
-      speed_val_right = 1750;
-    }
-  
-    if (speed_val_right < 1000) {
-      speed_val_right = 1000;
-    }
-
-  
-    left_font_motor.writeMicroseconds(speed_val_left);
-    left_rear_motor.writeMicroseconds(speed_val_left);
-  
-    right_rear_motor.writeMicroseconds(speed_val_right);
-    right_font_motor.writeMicroseconds(speed_val_right);
-
+    if (turnCount >= 3) {RunningState = STOP;}
+    else {RunningState = ROTATION;}   
   }
 }
+
 
 void Rotation() {
-
-      if (GYRO.getReading() < 85.00){ //ASDF
-          Serial.println(GYRO.getReading());
-          left_font_motor.writeMicroseconds(1500 + 250);
-          left_rear_motor.writeMicroseconds(1500 + 250);
-          right_rear_motor.writeMicroseconds(1500 + 250);
-          right_font_motor.writeMicroseconds(1500 + 250);
-          Serial.println(GYRO.getReading());
-      } 
-      else {
-          turnCount++;
-      RunningState = STRAIGHT;
-      }
-
-  
+  //Rotation P controller used to rotate the robot 90 degrees
+  rotateError = rotateTarget - GYRO.getReading();
+  if (rotateError > 5){
+    speed_val_rotate = constrain(1500 + R_Kp*rotateError, 1500 + min_power_rotate, 1500 + max_power_rotate);
+    SetMotorSpeed(speed_val_rotate, speed_val_rotate);
+  }
+  else {
+    //FSM state exit condition
+    turnCount++;
+    RunningState = SETUP;
+  }
 }
 
+
 void SetMotorSpeed(double speed_val_left, double speed_val_right) {
+  //Function to set robot motor speeds for the left and the right motors
   speed_val_left = constrain(speed_val_left, 750, 2250);
   speed_val_right = constrain(speed_val_right, 750, 2250);
 
@@ -172,6 +128,7 @@ void SetMotorSpeed(double speed_val_left, double speed_val_right) {
   right_rear_motor.writeMicroseconds(speed_val_right);
   right_font_motor.writeMicroseconds(speed_val_right);
 }
+
 
 void GetSensorReadings() {
   //Use in serial plotter with nothing else printing to serial
